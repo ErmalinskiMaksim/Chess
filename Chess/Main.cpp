7f0c152b572aa67ee6b2d53d576e7d73276d0dc0
Андрей Ямайка <maximka200205icloud.com>
2022-07-21 18:11:54 +0300

    A useless comment got removed

--- Chess/Main.cpp
+++ Chess/Main.cpp
@@ -1,20 +1,5 @@
 #include "Game.h"
 
-//			file map
-// 
-// Main				SFML/Graphics.hpp	
-//  |				  |
-//  V				  V
-// Game.h			Window.h ----> Entity.h
-// 	^							 /   |
-//  |							/    V
-//GameState.h <------Background.h	Piece.h
-//  |								|	^
-//  V								|	|
-//NewGameState.h <------------------|	|----PieceType.h <--- other crucial libs
-// 
-
-
 ///TO DO
 /// 1) Castling, En Passant, Promotion
 /// 2) Move list and navigation through the list
